642cda45159275be218b967401f4e4e56411b382
Ian Lockhart <lockhaim@gmail.com>
2022-03-17 14:50:42 -0600

    basic structure

--- linkaspen/src/App.js
+++ linkaspen/src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Ian Lockhart's LinkAspen!</h1>
+        <img className="main" src='https://media-exp1.licdn.com/dms/image/D5635AQGdJkjMyLDA9g/profile-framedphoto-shrink_200_200/0/1638893979703?e=1647637200&v=beta&t=sB8HDWnOabOtD0dUpqWFcJnUzSqyWF8H97EdQ-HK3Xo'></img>
+        <div className='mainLinks'>
+          <a>Link 1</a>
+          <a>Link 2</a>
+          <a>Link 3</a>
+          <a>Link 4</a>
+        </div>
+        <div className='socials'>
+          <button>Link 1</button>
+          <button>Link 2</button>
+          <button>Link 3</button>
+          <button>Link 4</button>
+        </div>
     </div>
   );
 }
